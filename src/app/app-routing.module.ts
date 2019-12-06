1b66b7950c399e9cbec8ca07cb466706ab38eeae
xmarti2 <xmarti2@lsu.edu>
2019-12-06 09:31:13 -0600

    next commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'list', // JDavis: changing from 'home' to 'list'.
+    redirectTo: 'home', // JDavis: changing from 'home' to 'list'.
     pathMatch: 'full'
   },
   {
